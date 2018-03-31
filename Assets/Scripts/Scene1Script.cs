c81de64042a7c74cf196a6852f36fe70f2e5c58e
zizhongtank <32141317+zizhongtank@users.noreply.github.com>
2018-03-31 07:02:29 -0400

    Initial commit

---
+++ Assets/Scripts/Scene1Script.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Scene1Script : MonoBehaviour
+{
+
+    //name  
+    private string mName = "Tank";
+    //age 
+    private int mAge = 27;
+    //grade 
+    private float mGrade = 90F;
+
+    void OnGUI()
+    {
+        GUILayout.Label("Unity3D data storage  program", GUILayout.Height(25));
+        //name  
+        GUILayout.Label("Please type in your name：", GUILayout.Height(25));
+        mName = GUILayout.TextField(mName, GUILayout.Height(25));
+        //age  
+        GUILayout.Label("Please type in your age：", GUILayout.Height(25));
+        mAge = int.Parse(GUILayout.TextField(mAge.ToString(), GUILayout.Height(25)));
+        //grade  
+        GUILayout.Label("Please type in your grade：", GUILayout.Height(25));
+        mGrade = float.Parse(GUILayout.TextField(mGrade.ToString(), GUILayout.Height(25)));
+
+        //submit data 
+        if (GUILayout.Button("Submit Data", GUILayout.Height(25)))
+        {
+            //clear data 
+            PlayerPrefs.SetString("Name", mName);
+            PlayerPrefs.SetInt("Age", mAge);
+            PlayerPrefs.SetFloat("Grade", mGrade);
+
+            //switch to Scene2  
+            SceneManager.LoadScene("Scene2");
+        }
+    }
+}
---
+++ Assets/Scripts/Scene2Script.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene2Script : MonoBehaviour
+{
+
+    private string mName;
+    private int mAge;
+    private float mGrade;
+
+    void Start()
+    {
+        //Read Datas 
+        mName = PlayerPrefs.GetString("Name", "DefaultValue");
+        mAge = PlayerPrefs.GetInt("Age", 0);
+        mGrade = PlayerPrefs.GetFloat("Grade", 0F);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Unity3D data storage  program", GUILayout.Height(25));
+        //name 
+        GUILayout.Label("Name：" + mName, GUILayout.Height(25));
+        //age
+        GUILayout.Label("Age：" + mAge, GUILayout.Height(25));
+        //grade 
+        GUILayout.Label("Grade：" + mGrade, GUILayout.Height(25));
+
+        //clear data
+        if (GUILayout.Button("Clear Data", GUILayout.Height(25)))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+
+        //return Scene1 
+        if (GUILayout.Button("Return Scene", GUILayout.Height(25)))
+        {
+           SceneManager.LoadScene("Scene1");
+        }
+
+    }
+}
